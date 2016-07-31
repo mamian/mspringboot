d142b0cefe23a0fca08f1b5a2b64eec5a25c94a7
mamian <mamianskyma@aliyun.com>
2016-07-31 23:53:19 +0800

    clear

--- src/main/java/net/mamian/mySpringboot/WebJspApplication.java
+++ src/main/java/net/mamian/mySpringboot/WebJspApplication.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package net.mamian.mySpringboot;
 
 import org.springframework.boot.SpringApplication;
