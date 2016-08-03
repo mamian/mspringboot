64366bdfb0647d236c801d61a0ac4043b1b7d8c1
mamian <mamianskyma@aliyun.com>
2016-08-03 20:31:05 +0800

    增加api访问者

---
+++ src/main/java/net/mamian/mySpringboot/common/Visitor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.mamian.mySpringboot.common;
+
+/**
+ * 访问者
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-8-3 19:45:14
+ * @copyright ©2016 马面 All Rights Reserved
+ */
+public enum Visitor {
+
+    PC,
+    
+    H5,
+    
+    IOS,
+    
+    ANDROID,
+    
+    ;
+    
+}
