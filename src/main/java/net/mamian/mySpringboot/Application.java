d8a247acf051a599a065d6adc512c8f9bd22753a
mamian <mamianskyma@aliyun.com>
2017-02-11 21:59:10 +0800

    开启异步功能

--- src/main/java/net/mamian/mySpringboot/Application.java
+++ src/main/java/net/mamian/mySpringboot/Application.java
@@ -7,6 +7,7 @@ package net.mamian.mySpringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -18,6 +19,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @copyright ©2016 马面 All Rights Reserved
  */
 @EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class Application {
 
