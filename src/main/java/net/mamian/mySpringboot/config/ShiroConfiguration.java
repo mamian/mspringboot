fc5b6171264c2f006e564615f38b74e5c578620c
mamian <mamianskyma@aliyun.com>
2017-03-15 21:48:34 +0800

    shiro

---
+++ src/main/java/net/mamian/mySpringboot/config/ShiroConfiguration.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.mamian.mySpringboot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Shiro 配置
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-7-26 10:02:39
+ * @copyright ©2016 马面 All Rights Reserved
+ */
+@Configuration
+public class ShiroConfiguration {
+
+    
+}
