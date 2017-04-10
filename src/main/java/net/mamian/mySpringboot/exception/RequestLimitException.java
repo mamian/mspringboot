6014158ac3ef558ea0fdc47a0aea6596fe1b9476
mamian <mamianskyma@aliyun.com>
2017-04-10 22:39:27 +0800

    exception

---
+++ src/main/java/net/mamian/mySpringboot/exception/RequestLimitException.java
@@ -0,0 +1,17 @@
+package net.mamian.mySpringboot.exception;
+
+/**
+ * Created by mamian on 17/4/8.
+ */
+public class RequestLimitException extends Exception {
+    private static final long serialVersionUID = 1364225358754654702L;
+
+    public RequestLimitException() {
+        super("HTTP请求超出设定的限制");
+    }
+
+    public RequestLimitException(String message) {
+        super(message);
+    }
+
+}
