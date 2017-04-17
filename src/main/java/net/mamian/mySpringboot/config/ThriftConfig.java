86711c24a97bec59455cb3ea7af42c0ce52d37b7
mamian <mamianskyma@aliyun.com>
2017-04-17 22:58:06 +0800

    thrift

---
+++ src/main/java/net/mamian/mySpringboot/config/ThriftConfig.java
@@ -0,0 +1,58 @@
+package net.mamian.mySpringboot.config;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import javax.annotation.PostConstruct;
+import net.mamian.mySpringboot.service.thrift.DataServiceImpl;
+import net.mamian.mySpringboot.thrift.service.DataService;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TTransportException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2017-04-15 19:51
+ * @copyright ©2017 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+@Configuration
+public class ThriftConfig {
+
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    @Bean
+    public TServerTransport tServerTransport() {
+        try {
+            return new TServerSocket(7911);
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Bean
+    public TServer tServer() {
+        //发布服务
+        DataService.Processor processor = new DataService.Processor(
+                new DataServiceImpl());
+        TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(
+                tServerTransport()).processor(processor));
+        return server;
+    }
+
+    @PostConstruct
+    public void init() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                tServer().serve();
+            }
+        });
+    }
+
+}
